a73a9f8ecf0d2c2bf918d47483fc15abe5e82df8
Ibrahyma <i.missoma@gmail.com>
2021-04-21 19:43:21 +0200

    Reset design, fix bugs

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,6 +1,4 @@
-import React, { Component } from "react"
-import Category from "../components/Category"
-import Post from "../components/Post"
+import { Component } from "react"
 import { getRessources } from "../services/api_service"
 
 export default class Home extends Component {
@@ -24,28 +22,7 @@ export default class Home extends Component {
     }
 
     render() {
-        const { posts, categories } = this.state
-
         return [
-            <h1>Home</h1>,
-            <div className="row">
-                <div className="leftcolumn">
-                    <h2>Categories</h2>
-                    {
-                        categories.map(post => (
-                            <Category itemId={ post._id }/>
-                        ))
-                    }
-                </div>
-                <div className="rightcolumn">
-                    <h2>Posts</h2>
-                    {
-                        posts.map(post => (
-                            <Post itemId={ post._id }/>
-                        ))
-                    }
-                </div>
-            </div>
         ]
     } 
 }
